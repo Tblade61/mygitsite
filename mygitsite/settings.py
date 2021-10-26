58b7f48bae4674779dc92bb5ebd23adefe13aba4
Mats Thornblad <mats.thornblad@hotmail.com>
2021-10-26 15:16:09 +0200

    Added Procfile

--- mygitsite/settings.py
+++ mygitsite/settings.py
@@ -122,6 +122,8 @@ STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+#STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
